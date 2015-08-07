6250266fc81b37a9c653973117327cb1318f068c
Florent Teuber <fteuber@gmail.com>
2015-08-07 13:46:09 +0200

    changed visibility of emarsys base url

--- src/Snowcap/Emarsys/Client.php
+++ src/Snowcap/Emarsys/Client.php
@@ -21,7 +21,7 @@ class Client
     /**
      * @var string
      */
-    private $baseUrl = 'https://suite6.emarsys.net/api/v2/';
+    protected $baseUrl = 'https://suite6.emarsys.net/api/v2/';
     /**
      * @var string
      */
